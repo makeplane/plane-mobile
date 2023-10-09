e245d2f895a5a51595e03ec5d80a00315e840273
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2023-10-09 19:02:13 +0530

    fix: module detail lead/member sheet fix, estimate enabling/disabling fix, spreadsheet view alignment fix

--- lib/provider/projects_provider.dart
+++ lib/provider/projects_provider.dart
@@ -461,6 +461,7 @@ class ProjectsProvider extends ChangeNotifier {
       projects[index]["description"] = currentProject["description"];
       projects[index]["identifier"] = currentProject["identifier"];
       projects[index]["emoji"] = currentProject["emoji"];
+      projects[index]['estimate'] = currentProject['estimate'];
 
       projects[index]["icon_prop"] = currentProject["icon_prop"] != null
           ? {
